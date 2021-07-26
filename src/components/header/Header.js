4023c4eb588f3b6cb309e84a5fb7089a8809d4b2
gabey14 <abeygeorge14@gmail.com>
2021-07-26 19:15:15 +0530

    first commit

---
+++ src/components/header/Header.js
@@ -0,0 +1,23 @@
+function Header() {
+  return (
+    <>
+      <header className="banner">
+        <div className="banner__contents">
+          <h1 className="banner__title">The Witcher</h1>
+          <div className="banner__buttons">
+            <button className="banner__button">Play</button>
+            <button className="banner__button">My List</button>
+          </div>
+          <h1 className="banner__description">
+            The witcher Geralt, a mutated monster hunter, struggles to find his
+            place in a world in which people often prove more wicked than
+            beasts.
+          </h1>
+        </div>
+        <div className="banner--fadeBottom"></div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
---
+++ src/components/nav/Nav.js
@@ -0,0 +1,20 @@
+function Nav() {
+  return (
+    <>
+      <div className="nav false">
+        <img
+          src="https://www.freepnglogos.com/uploads/netflix-logo-0.png"
+          alt="logo of netflix"
+          className="nav__logo"
+        />
+        <img
+          src="http://pngimg.com/uploads/netflix/netflix_PNG8.png"
+          alt="logo of netflix"
+          className="nav__avatar"
+        />
+      </div>
+    </>
+  );
+}
+
+export default Nav;
